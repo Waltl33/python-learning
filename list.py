92557cb5e75d0d315d9c1449114264ff12d0df3e
Walter <walter.clark24@yahoo.com>
2023-10-24 11:08:48 -0400

    tuples

--- list.py
+++ list.py
@@ -2,3 +2,4 @@ countries = ['United Kingdom', 'Ghana', 'Nigeria', 'Austrailia', 'New Zealand']
 countries2 = list(("Nigeria", 34, False))
 
 print(countries)
+
---
+++ tuples.py
@@ -0,0 +1,3 @@
+three_numbers = (1, 2, 3)
+three_numbers[1] = 23
+print(three_numbers[0])
